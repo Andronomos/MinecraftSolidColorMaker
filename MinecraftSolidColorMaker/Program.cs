d5d1b01662ff3054051179ac8d259f12bb2df9c1
tim-dm <andronomos@inbox.com>
2022-05-24 14:13:43 -0700

    .

--- MinecraftSolidColorMaker/Program.cs
+++ MinecraftSolidColorMaker/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace MinecraftSolidColorMaker
@@ -7,9 +11,44 @@ namespace MinecraftSolidColorMaker
     {
         static void Main(string[] args)
         {
-            if (args.Length != 0)
-                if (Directory.Exists(args[0]))
-                    workPath = args[0];
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid args");
+                return;
+            }
+
+            List<string> textures = new();
+
+            if (Directory.Exists(args[0]))
+            {
+                textures.AddRange(Directory.GetFiles(args[0], "*.png"));
+            }
+
+            ProcessTextures(textures);
         }
+
+
+
+        private static void ProcessTextures(List<string> textures)
+        {
+            foreach (string texture in textures)
+            {
+                using (Image<Rgba32> img = Image.Load<Rgba32>(texture))
+                {
+                    Rgba32 color = img[0, 0];
+                }
+            }
+        }
+
+        private void CreateNewTexture()
+        {
+            using (var newTexture = new Image<Rgba32>(16, 16))
+            {
+                //newTexture.Mutate(i => i.);
+                //newTexture.
+            }
+        }
+
+
     }
 }
