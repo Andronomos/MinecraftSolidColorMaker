a0f71235c9a289a7b222e8960a67024dc59bdce3
tim-dm <andronomos@inbox.com>
2022-05-24 16:01:06 -0700

    .

--- MinecraftSolidColorMaker/Program.cs
+++ MinecraftSolidColorMaker/Program.cs
@@ -11,6 +11,8 @@ namespace MinecraftSolidColorMaker
     class Program
     {
         private const int TextureSize = 16;
+        private static string _savePath = Environment.CurrentDirectory;
+        private static bool _useOldTextureName = false;
 
         static void Main(string[] args)
         {
@@ -28,23 +30,69 @@ namespace MinecraftSolidColorMaker
                 textures.AddRange(Directory.GetFiles(args[0], "*.png"));
             }
 
+            string newSavePath = args[1];
+
+            if(!string.IsNullOrEmpty(newSavePath))
+            {
+                if(IsValidPath(newSavePath))
+                {
+                    _savePath = newSavePath;
+                    Directory.CreateDirectory(newSavePath);
+                }                
+            }
+
+            if(!string.IsNullOrEmpty(args[2]))
+            {
+                _useOldTextureName = args[2] == "--usetexturename";
+            }
+
             ProcessTextures(textures);
         }
 
         private static void ProcessTextures(List<string> textures)
         {
+            int textureCounter = 0;
+
             foreach (string texture in textures)
             {
-                using (Image<Rgba32> img = Image.Load<Rgba32>(texture))
+                if(File.Exists(texture))
                 {
-                    Rgba32 color = img[0, 0];
-
-                    using (var newTexture = new Image<Rgba32>(TextureSize, TextureSize, color))
+                    using (Image<Rgba32> img = Image.Load<Rgba32>(texture))
                     {
-                        newTexture.SaveAsPng(@"C:\Users\User\Desktop\1.png");
-                    }
+                        Rgba32 color = img[0, 0];
+
+                        using (var newTexture = new Image<Rgba32>(TextureSize, TextureSize, color))
+                        {
+                            string name = $"{textureCounter}";
+
+                            if (_useOldTextureName)
+                            {
+                                name = Path.GetFileNameWithoutExtension(texture);
+                            }
+                            else
+                            {
+                                textureCounter++;
+                            }
+
+                            newTexture.SaveAsPng(@$"{_savePath}\{name}.png");
+                        }
+                    }                    
                 }
             }
+        }
+
+        private static bool IsValidPath(string path)
+        {
+            try
+            {
+                Path.GetFullPath(path);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
