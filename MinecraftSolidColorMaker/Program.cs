d39304ceb3fad6a2bb073f4be7d8f517736bf063
tim-dm <andronomos@inbox.com>
2022-05-24 15:39:18 -0700

    .

--- MinecraftSolidColorMaker/Program.cs
+++ MinecraftSolidColorMaker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing.Extensions;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System;
@@ -9,11 +10,14 @@ namespace MinecraftSolidColorMaker
 {
     class Program
     {
+        private const int TextureSize = 16;
+
         static void Main(string[] args)
         {
             if (args.Length == 0)
             {
                 Console.WriteLine("Invalid args");
+                Console.ReadLine();
                 return;
             }
 
@@ -25,9 +29,9 @@ namespace MinecraftSolidColorMaker
             }
 
             ProcessTextures(textures);
+
+            Console.ReadLine();
         }
-
-
 
         private static void ProcessTextures(List<string> textures)
         {
@@ -36,19 +40,29 @@ namespace MinecraftSolidColorMaker
                 using (Image<Rgba32> img = Image.Load<Rgba32>(texture))
                 {
                     Rgba32 color = img[0, 0];
+
+                    //using (var img2 = img.Clone(ctx => ctx.(font, "A short piece of text", Color.HotPink, 5, false)))
+                    //{
+                    //    img2.Save("output/simple.png");
+                    //}
+
+
+                    CreateNewTexture(color);
+
+
                 }
             }
         }
 
-        private void CreateNewTexture()
+        private static void CreateNewTexture(Rgba32 color)
         {
-            using (var newTexture = new Image<Rgba32>(16, 16))
+            using (var newTexture = new Image<Rgba32>(TextureSize, TextureSize, color))
             {
                 //newTexture.Mutate(i => i.);
-                //newTexture.
+                //var rectangle = new RectangleF(0, 0, TextureSize, TextureSize);
+                //newTexture.Mutate(i => i.);
+                newTexture.SaveAsPng(@"C:\Users\User\Desktop\1.png");
             }
         }
-
-
     }
 }
