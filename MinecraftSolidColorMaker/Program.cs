86db464b0a3848ad094f9789ce642bdd2b357be1
tim-dm <andronomos@inbox.com>
2022-05-24 13:44:13 -0700

    .

---
+++ MinecraftSolidColorMaker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace MinecraftSolidColorMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+                if (Directory.Exists(args[0]))
+                    workPath = args[0];
+        }
+    }
+}
